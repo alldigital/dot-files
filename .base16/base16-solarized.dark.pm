9e99886dea25e2d2dc33791fbd90f64255fd4bb4
alldigital <edward@tulupnikov.com>
2016-07-04 16:24:35 +0300

    Solarized light theme

--- .base16/base16-solarized.dark.pm
+++ .base16/base16-solarized.dark.pm
@@ -1,13 +1,13 @@
 package base16;
 
-our $base00="#002b36";
+our $base00="#fdf6e3";
 our $base01="#073642";
-our $base02="#586e75";
+our $base02="#eee8d5";
 our $base03="#657b83";
 our $base04="#839496";
 our $base05="#93a1a1";
-our $base06="#eee8d5";
-our $base07="#fdf6e3";
+our $base06="#586e75";
+our $base07="#002b36";
 our $base08="#dc322f";
 our $base09="#cb4b16";
 our $base0A="#b58900";
