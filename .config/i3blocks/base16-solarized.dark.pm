70ea16aecec996b3f8d7f8353bb3e27a55a943f8
alldigital <edward@tulupnikov.com>
2017-01-11 15:03:32 +0300

    base16 solarized added to i3blocks

---
+++ .config/i3blocks/base16-solarized.dark.pm
@@ -0,0 +1,18 @@
+package base16;
+
+our $base00="#fdf6e3";
+our $base01="#073642";
+our $base02="#eee8d5";
+our $base03="#657b83";
+our $base04="#839496";
+our $base05="#93a1a1";
+our $base06="#586e75";
+our $base07="#002b36";
+our $base08="#dc322f";
+our $base09="#cb4b16";
+our $base0A="#b58900";
+our $base0B="#859900";
+our $base0C="#2aa198";
+our $base0D="#268bd2";
+our $base0E="#6c71c4";
+our $base0F="#d33682";
