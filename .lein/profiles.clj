a091a49a145cb812ef756ebba200a0b39d39042d
alldigital <edward@tulupnikov.com>
2017-04-06 19:50:43 +0300

    lein .profile.clj - lein-gorilla stale deps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.8.1"]
-                       [org.clojure/tools.nrepl "0.2.12"]
+                       ; [org.clojure/tools.nrepl "0.2.13"]
                        [org.clojure/clojure "1.8.0"]
                        [alembic "0.3.2"]
                        [jonase/eastwood "0.2.3"]]
@@ -7,13 +7,14 @@
                      (pjstadig.humane-test-output/activate!)]
         :plugins [[lein-ancient "0.6.10"]
                   [lein-pprint "1.1.2"]
-                  [com.jakemccrary/lein-test-refresh "0.17.0"]
+                  [com.jakemccrary/lein-test-refresh "0.19.0"]
                   [lein-try "0.4.3"]
                   [lein-kibit "0.1.3"]
                   [lein-deps-tree "0.1.2"]
                   [lein-cljfmt "0.5.6"]
                   [lein-exec "0.3.6"]
-                  [lein-gorilla "0.4.0"]]
+;                  [lein-gorilla "0.4.0"]
+                  ]
         :gorilla-options
         {:keymap {"command:worksheet:newBelow" "alt+o"
                     "command:worksheet:newAbove" "alt+shift+o"}
