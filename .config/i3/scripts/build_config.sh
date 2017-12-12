4fb717b0f3abf2ebf9d9c3467c5054e9e0e33904
alldigital <edward@tulupnikov.com>
2017-12-12 13:55:31 +0300

    Clean up i3 config. Colors refactored. Switched to material theme.

--- .config/i3/scripts/build_config.sh
+++ .config/i3/scripts/build_config.sh
@@ -3,17 +3,10 @@
 HOSTNAME=$(hostname)
 
 I3=$HOME/.config/i3
-DARK=$HOME/.dark
 
 function build() {
     cd ${I3}
     cat configs/config.base > config
-
-    if [ -f $DARK ]; then
-        ln -sf ${I3}/colors/solarized.dark colors/colors
-    else
-        ln -sf ${I3}/colors/solarized.light colors/colors
-    fi
 
     cat colors/colors colors/base >> config
     cat keybindings/* >> config
