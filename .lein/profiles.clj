7ad9d3cbb594dd5736b34d980d9a35c19d73e724
alldigital <edward@tulupnikov.com>
2018-01-15 10:26:05 +0300

    Lein profile additional deps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,15 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.8.3"]
-                       ; [org.clojure/tools.nrepl "0.2.13"]
                        [org.clojure/clojure "1.9.0"]
+                       [vvvvalvalval/scope-capture "0.1.4"]
                        [alembic "0.3.2"]
+                       [spyscope "0.1.5"]
                        [jonase/eastwood "0.2.5"]]
         :injections [(require 'pjstadig.humane-test-output)
+                     (require 'sc.api)
+                     (require 'spyscope.core)
                      (pjstadig.humane-test-output/activate!)]
         :plugins [[lein-ancient "0.6.15"]
+                  [me.arrdem/lein-git-version "2.0.4"]
                   [lein-pprint "1.2.0"]
                   [com.jakemccrary/lein-test-refresh "0.22.0"]
                   [lein-try "0.4.3"]
