fcb634974f5d8f36648938aa2e4a405a09144eea
alldigital <edward@tulupnikov.com>
2018-01-16 10:25:49 +0300

    Add bash time to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,24 @@
+fish_vi_key_bindings
+
 if test $TERM = "eterm-256color";
     set fish_term24bit 0
 else
     set -gx TERM xterm-256color
 end
 
+# Override default prompt colors
+set -g theme_color_scheme terminal2
+
 set PATH $HOME/bin $PATH
+
+function sudo
+  if test "$argv" = !!
+    eval command sudo $history[1]
+  else
+    command sudo $argv
+  end
+end
+
+function time --description="Time just like in Bash"
+  command time --portability $argv
+end
